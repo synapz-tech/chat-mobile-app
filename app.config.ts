cc1322dad858b3f566fbf0836e245c54af71b7b0
cesaraugusto <cesar.jose@petzpartner.com.br>
2025-10-03 15:20:53 -0300

    fix: Correct bundleIdentifier in app.config.ts from 'com.synpaz.chat' to 'com.synapz.chat' 🔧

--- app.config.ts
+++ app.config.ts
@@ -17,7 +17,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     },
     ios: {
       supportsTablet: true,
-      bundleIdentifier: 'com.synpaz.chat',
+      bundleIdentifier: 'com.synapz.chat',
       infoPlist: {
         NSCameraUsageDescription:
           'This app requires access to the camera to upload images and videos.',
