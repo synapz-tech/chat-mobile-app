410a6bbf6dc36aeccdb14203647301af898add10
cesaraugusto <cesar.jose@petzpartner.com.br>
2025-10-05 09:11:07 -0300

    feat: Add .watchmanconfig and update firebase.json for analytics configuration 📊
    fix: Remove unnecessary text in LoginScreen component's Pressable element ✂️

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -236,11 +236,7 @@ const LoginScreen = () => {
 
           <Pressable
             style={tailwind.style('flex-row justify-center items-center mt-6')}
-            onPress={openConfigInstallationURL}>
-            <Animated.Text style={tailwind.style('text-sm text-gray-900')}>
-              {i18n.t('LOGIN.CHANGE_URL')}
-            </Animated.Text>
-          </Pressable>
+            onPress={openConfigInstallationURL}></Pressable>
           <Pressable
             style={tailwind.style('flex-row justify-center items-center mt-4')}
             onPress={() => languagesModalSheetRef.current?.present()}>
